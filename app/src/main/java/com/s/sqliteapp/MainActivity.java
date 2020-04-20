39ed302437862b6967db7e962e3f25e232583802
Yasir Akram <codingWithyasir.github.com>
2020-04-20 23:12:59 +0500

    Initial commit

---
+++ app/src/main/java/com/s/sqliteapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.s.sqliteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.s.sqliteapp.RecyclerPakage.DbModelClass;
+import com.s.sqliteapp.SQLitePakage.MyDbClass;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyDbClass objMyDbClass;
+    ArrayList<DbModelClass> objDbModelClassArrayList;
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        objMyDbClass=new MyDbClass(this);
+        objDbModelClassArrayList=new ArrayList<>();
+        recyclerView=findViewById(R.id.DataRV);
+    }
+
+    public void showData(View view)
+    {
+        try 
+        {
+             objDbModelClassArrayList=objMyDbClass.getAllData();
+             DbAdapter objDbAdapter=new DbAdapter(objDbModelClassArrayList);
+
+             recyclerView.hasFixedSize();
+             recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+             recyclerView.setAdapter(objDbAdapter);
+            
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "show data.....", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
